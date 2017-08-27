a2b2c593af82148722fdc0b14afec049a60d7e8e
elpinal <elpinal@users.noreply.github.com>
2017-08-27 22:29:30 +0900

    Add test

---
+++ types_test.go
@@ -0,0 +1,24 @@
+package types
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTypeFTV(t *testing.T) {
+	tests := []struct {
+		t    Type
+		want []string
+	}{
+		{
+			t:    &TVar{name: "a"},
+			want: []string{"a"},
+		},
+	}
+	for _, test := range tests {
+		got := test.t.ftv()
+		if !reflect.DeepEqual(got, test.want) {
+			t.Errorf("ftv on Type: got %v, want %v", got, test.want)
+		}
+	}
+}
